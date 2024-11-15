fb54b5fa0953a7899936f91e3a40d818adb95687
Wen Junhua <wenjh1998@gmail.com>
2024-11-16 00:38:06 +0800

    chore: convert to typescript

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM, { Container } from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { App } from './App.tsx';
+import reportWebVitals from './reportWebVitals.ts';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const rootElem = document.getElementById('root');
+const root = ReactDOM.createRoot(rootElem as Container);
+
 root.render(
   <React.StrictMode>
     <App />
@@ -14,4 +16,4 @@ root.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals(console.log);
