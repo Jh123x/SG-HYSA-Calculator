709d523da223ccffb0d3f35feeaf45f2a26561ae
Wen Junhua <wenjh1998@gmail.com>
2024-12-02 00:15:19 +0800

    fix: update maribank calc

---
+++ src/logic/maribank.test.ts
@@ -0,0 +1,16 @@
+import { ResultInterest } from "../types/interest_result";
+import { NewProfile } from "../types/profile";
+import { maribank_interest } from "./maribank";
+
+describe("Maribank interest rates", () => {
+    for (var i = 0; i < 200_000; i+=10000) {
+        it(`balance ${i} should be correct`, () => {
+            const result = maribank_interest(NewProfile({ Savings: i, }))
+            if (i > 100_000){
+                expect(result).toEqual(new ResultInterest(100_000 * 0.027))
+            } else {
+                expect(result).toEqual(new ResultInterest(i * 0.027))
+            }
+        })
+    }
+})
--- src/logic/maribank.ts
+++ src/logic/maribank.ts
@@ -3,6 +3,10 @@ import { ResultInterest } from "../types/interest_result.ts";
 import Profile from "../types/profile.ts";
 
 export const maribank_interest = (profile: Profile): ResultInterest => {
+    if (profile.Savings > 100_000){
+        profile.Savings = 100_000
+    }
+
     return calculate_ir(profile.Savings, {
         cutoffs: [],
         baseRatePercent: 2.7,
