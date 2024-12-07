670f1a55f32ecbd529a260ba8981d725873478be
Wen Junhua <wenjh1998@gmail.com>
2024-12-07 22:22:16 +0800

    test: add test for choco finance

---
+++ src/logic/choco_finanace.test.ts
@@ -0,0 +1,40 @@
+import { ResultInterest } from "../types/interest_result";
+import { NewProfile } from "../types/profile";
+import { choco_finance } from "./choco_finance";
+
+describe("Choco Finance", () => {
+    const tests = [
+        {
+            name: "should return the correct i/r for < 20k",
+            savings: 1000,
+            expected_ir: 36,
+        },
+        {
+            name: "should return the correct i/r for 20k",
+            savings: 20000,
+            expected_ir: 720,
+        },
+        {
+            name: "should return the correct i/r for > 20k and < 50k",
+            savings: 30000,
+            expected_ir: 1040,
+        },
+        {
+            name: "should return the correct i/r for 50k",
+            savings: 50000,
+            expected_ir: 1680,
+        },
+        {
+            name: "should return the i/r for 50k for > 50k",
+            savings: 100000,
+            expected_ir: 1680,
+        }
+    ]
+
+    for (const test of tests) {
+        it(test.name, () => {
+            const result = choco_finance(NewProfile({ Savings: test.savings }))
+            expect(result).toEqual(new ResultInterest(test.expected_ir))
+        })
+    }
+})
