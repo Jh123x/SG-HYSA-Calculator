1bd7cacc09f8ca21bf0e528a333824e0df21fd03
Wen Junhua <wenjh1998@gmail.com>
2025-03-31 10:59:39 +0800

    test: add test for alerts

---
+++ src/Components/Alert.test.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { render } from "@testing-library/react";
+import { WebAlert } from "./Alert";
+
+describe("Web Alert", () => {
+    it("should match snapshot", () => {
+        const tree = render(<WebAlert 
+            severity="success"
+            hideModel={false}
+            onClose={() => { }}
+            children="Test Alert"
+        />)
+        expect(tree).toMatchSnapshot()
+    })
+})
