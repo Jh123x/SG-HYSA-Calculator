e8f85e2056aae6e01f6c6e36976c41fd0856111f
Wen Junhua <wenjh1998@gmail.com>
2025-07-10 23:45:49 +0800

    feat: update standard chartered interest rates

--- src/logic/stand_chart.test.ts
+++ src/logic/stand_chart.test.ts
@@ -1,8 +1,86 @@
 import { ResultInterest } from "../types/interest_result"
-import { stand_chart_interest } from "./stand_chart"
+import { stand_chart_interest_before_06_25, stand_chart_interest } from "./stand_chart"
 import Profile, { NewProfile } from "../types/profile"
 
-describe("Standard Chartered Interest Rates", () => {
+describe("Standard Chartered I/R", () => {
+    const tests: {
+        name: string
+        profile: Profile
+        expectedInterest: ResultInterest
+    }[] = [
+            {
+                name: "should be correct for 100k balance with all interest rates",
+                profile: NewProfile({
+                    Savings: 100000,
+                    Salary: 3000,
+                    Spending: 1000,
+                    Investment: 20000,
+                    Insurance: 12000,
+                }),
+                expectedInterest: new ResultInterest(8050, 100000)
+            },
+            {
+                name: "should be correct for 200k balance with all interest rates",
+                profile: NewProfile({
+                    Savings: 200000,
+                    Salary: 3000,
+                    Spending: 2000,
+                    Investment: 30000,
+                    Insurance: 12000,
+                }),
+                expectedInterest: new ResultInterest(8100, 200000)
+            },
+            {
+                name: "should be correct for 200k no condition",
+                profile: NewProfile({ Savings: 200000 }),
+                expectedInterest: new ResultInterest(100, 200000),
+            },
+            {
+                name: "should be correct for 100k balance with 2 req",
+                profile: NewProfile({
+                    Savings: 100000,
+                    Salary: 3000,
+                    Spending: 1000,
+                }),
+                expectedInterest: new ResultInterest(3050, 100000)
+            },
+            {
+                name: "should be correct for 100k balance with no condition",
+                profile: NewProfile({ Savings: 100_000, }),
+                expectedInterest: new ResultInterest(50, 100_000)
+            },
+            {
+                name: "should be correct for 100k balance with salary only",
+                profile: NewProfile({
+                    Savings: 100000,
+                    Salary: 3000,
+                }),
+                expectedInterest: new ResultInterest(1550, 100000)
+            },
+            {
+                name: "should be correct for 50k balance with salary only",
+                profile: NewProfile({
+                    Savings: 50000,
+                    Salary: 3000,
+                }),
+                expectedInterest: new ResultInterest(775, 50000),
+            },
+            {
+                name: "no money should have no interest",
+                profile: NewProfile({}),
+                expectedInterest: new ResultInterest(0, 0)
+            }
+        ]
+
+    for (const testCase of tests) {
+        it(testCase.name, () => {
+            const result = stand_chart_interest(testCase.profile)
+            expect(result).toEqual(testCase.expectedInterest)
+        })
+    }
+})
+
+describe("Standard Chartered Pre 06 25 Interest Rates", () => {
     const tests: {
         name: string
         profile: Profile
@@ -75,7 +153,7 @@ describe("Standard Chartered Interest Rates", () => {
 
     for (const testCase of tests) {
         it(testCase.name, () => {
-            const result = stand_chart_interest(testCase.profile)
+            const result = stand_chart_interest_before_06_25(testCase.profile)
             expect(result).toEqual(testCase.expectedInterest)
         })
     }
--- src/logic/stand_chart.ts
+++ src/logic/stand_chart.ts
@@ -8,6 +8,24 @@ export const stand_chart_interest = (profile: Profile): ResultInterest => {
     const { Savings, Salary, Spending, Insurance, Investment } = profile
     var interest = baseInterest
 
+    if (Salary >= 3000) interest += 1.5
+    if (Spending >= 1000) interest += 1.5
+    if (Insurance >= 12000) interest += 2.5
+    if (Investment >= 20000) interest += 2.5
+
+    return calculate_ir(
+        Savings,
+        {
+            cutoffs: [{ Cutoff: 100_000, InterestRatePercent: interest }],
+            baseRatePercent: baseInterest
+        },
+    )
+}
+
+export const stand_chart_interest_before_06_25 = (profile: Profile): ResultInterest => {
+    const { Savings, Salary, Spending, Insurance, Investment } = profile
+    var interest = baseInterest
+
     if (Salary >= 3000) interest += 1
     if (Spending >= 1000) interest += 1
     if (Insurance >= 12000) interest += 2
