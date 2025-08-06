8e8f4ca6487a2a8dd70f6f04acda681c456a8339
Wen Junhua <wenjh1998@gmail.com>
2025-08-06 23:18:52 +0800

    feat: update ads determination factor

--- src/Components/Ads.tsx
+++ src/Components/Ads.tsx
@@ -2,10 +2,8 @@ import React from "react";
 
 
 export const Ads = () => {
-    const [showAds, setShowAds] = React.useState(false);
-    React.useEffect(() => { }, []);
-
-    if (showAds) return <></>
+    const showAds = localStorage.getItem('showAds') !== 'false';
+    if (!showAds) return <></>
 
     const script = document.createElement('script');
     script.src = "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1199924535485592"
