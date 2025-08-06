14c0dd61b4e2560cc9490bf5eed7276abc2836c7
Wen Junhua <wenjh1998@gmail.com>
2025-08-06 23:40:05 +0800

    feat: update ads

--- src/Components/Ads.tsx
+++ src/Components/Ads.tsx
@@ -3,13 +3,7 @@ import React from "react";
 
 export const Ads = () => {
     const showAds = localStorage.getItem('showAds') !== 'false';
-    if (!showAds) return <></>
 
-    const script = document.createElement('script');
-    script.src = "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1199924535485592"
-    script.async = true;
-    script.defer = true;
-    script.crossOrigin = "anonymous";
-
-    return <>{script}</>
+    if (!showAds) document.getElementById('ads')?.remove();
+    return <></>
 }
