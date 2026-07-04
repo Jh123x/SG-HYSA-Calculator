280c3035c6f51fd96584e14933e27bdb5193bd58
Junhua Wen <junhua@jh123x.com>
2026-07-04 15:54:10 +0000

    fix: set module:esnext and allowSyntheticDefaultImports for dynamic imports

--- src/pages/BankDetailPage.tsx
+++ src/pages/BankDetailPage.tsx
@@ -300,3 +300,5 @@ export const BankDetailPage = ({ profile }: BankDetailPageProps) => {
     </>
   );
 };
+
+export default BankDetailPage;
--- src/pages/FaqPage.tsx
+++ src/pages/FaqPage.tsx
@@ -281,3 +281,5 @@ export const FaqPage = () => {
     </>
   );
 };
+
+export default FaqPage;
