ff6c7827366bc6d3633a2990fe36b57d93ea3031
Wen Junhua <wenjh1998@gmail.com>
2026-07-20 23:15:03 +0800

    test: improve test coverage

---
+++ src/Components/ErrorBoundary.test.tsx
@@ -0,0 +1,19 @@
+import { describe, it, expect } from "vitest";
+import { render } from "@testing-library/react";
+import { ErrorBoundary } from "./ErrorBoundary";
+
+
+const ErrComponent = () => {
+  throw Error("test error")
+}
+
+describe("ErrorBoundary", () => {
+  it("should match snapshot for no error", () => {
+    const { asFragment } = render(<ErrorBoundary><div></div></ErrorBoundary>)
+    expect(asFragment()).toMatchSnapshot();
+  });
+  it("should match snapshot on error", () => {
+    const { asFragment } = render(<ErrorBoundary><ErrComponent /></ErrorBoundary>)
+    expect(asFragment()).toMatchSnapshot();
+  })
+})
